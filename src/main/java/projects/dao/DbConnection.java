6b0899630e53a66dc4dace8573dfccf4d93efcc9
Jared <idku@duck.com>
2024-02-17 13:16:34 -0800

    first commit

--- src/main/java/projects/dao/DbConnection.java
+++ src/main/java/projects/dao/DbConnection.java
@@ -9,17 +9,17 @@ import recipes.exception.DbException;
 public class DbConnection {
 
 	private static final String SCHEMA = "projects";
-	private static final String USER = "recipes";
-	private static final String PASSWORD = "recipes";
+	private static final String USER = "projects";
+	private static final String PASSWORD = "projects";
 	private static final String HOST = "localhost";
 	private static final int PORT = 3306;
 	
 	
 	public static Connection getConnection() {
-	String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", 
+	String url = 
+			String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", 
 			HOST, PORT, SCHEMA, USER, PASSWORD);
 			
-	System.out.println("Unable to get connection at " + url);
 			
 	try {
 		Connection conn = DriverManager.getConnection(url);
